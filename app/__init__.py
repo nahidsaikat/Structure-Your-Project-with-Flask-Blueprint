a76f503441bc59c8dcbc4816cff6e70c110a6e57
nahid_craftsmen <nahidur.rahman@m2amedia.tv>
2020-04-01 22:06:42 +0600

    Create settings.py file and using it as config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,14 @@
-import os
-
 from flask import Flask, Blueprint
 
 from .extensions import db, ma
 from app.note.views import note_blueprint
 
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-
-def create_app():
+def create_app(config_file='settings.py'):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' +  os.path.join(BASE_DIR, 'db.sqlite3')
+
+    app.config.from_pyfile(config_file)
 
     db.init_app(app)
     ma.init_app(app)
